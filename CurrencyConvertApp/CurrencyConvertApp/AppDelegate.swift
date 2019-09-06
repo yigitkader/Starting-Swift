dfc075f2d42fb5618e85948f6e97e9feb6418fc8
yigitkader <ryigitkader@gmail.com>
2019-09-06 18:00:28 +0300

    currency convert project created

--- CurrencyConvertApp/CurrencyConvertApp/AppDelegate.swift
+++ CurrencyConvertApp/CurrencyConvertApp/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  CustomizeKey
+//  CurrencyConvertApp
 //
-//  Created by calzom on 5.09.2019.
+//  Created by calzom on 6.09.2019.
 //  Copyright © 2019 YigitKader. All rights reserved.
 //
 
