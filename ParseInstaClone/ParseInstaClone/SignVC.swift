87059488fe911b48447ad43cf51a986e6b88453c
yigitkader <ryigitkader@gmail.com>
2019-09-09 20:32:18 +0300

    user sign operation created

--- ParseInstaClone/ParseInstaClone/SignVC.swift
+++ ParseInstaClone/ParseInstaClone/SignVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+
 
 class SignVC: UIViewController {
 
@@ -18,16 +20,139 @@ class SignVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /* SAVE DATAS
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "cherry"
+        parseObject["calories"] = "20"
+        
+        parseObject.saveInBackground { (success, error) in
+            
+            if error != nil {
+                
+                print(error?.localizedDescription)
+            }
+            else{
+                print("saved")
+            }
+        } */
+        
+        
+        
+        
+        
+       /* BRING DATAS
+        let query = PFQuery(className: "Fruits")
+        
+        //filter
+        query.whereKey("calories", greaterThan: "30")
+        //query.whereKey("name", equalTo: "cherry")
+        
+        query.findObjectsInBackground { (objects, error) in
+            
+            
+            if error != nil {
+                
+                print(error?.localizedDescription)
+            }
+            else{
+                print(objects!)
+            }
+        } */
+        
+        
+        
+        
     }
     
 
     @IBAction func signInButton(_ sender: Any) {
-        performSegue(withIdentifier: "toTabBar", sender: nil)
+        
+        let user = PFUser()
+        
+        
+        if signNickLabel.text != "" && signPassLabel.text != "" {
+            
+            user.username = signNickLabel.text
+            user.password = signPassLabel.text
+            
+            PFUser.logInWithUsername(inBackground: signNickLabel.text!, password: signPassLabel.text!) { (user, error) in
+                
+                if error != nil {
+                    
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    
+                    let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+                    
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else{
+                    
+                    self.performSegue(withIdentifier: "toTabBar", sender: nil)
+                    
+                }
+            }
+            
+            
+        }
+        else{
+            
+            let alert = UIAlertController(title: "Error", message: "Do not use blank charachters", preferredStyle: UIAlertController.Style.alert)
+            
+            let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
+        
+        
+        
     }
     
     
     @IBAction func signUpButton(_ sender: Any) {
+        
+        let user = PFUser()
+      
+        
+        if signNickLabel.text != "" && signPassLabel.text != "" {
+            
+            user.username = signNickLabel.text
+            user.password = signPassLabel.text
+            
+            user.signUpInBackground { (success, error) in
+                
+                if error != nil {
+                    
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    
+                    let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+                    
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else{//no error
+                    
+                    self.performSegue(withIdentifier: "toTabBar", sender: nil)
+                    
+                }
+            }
+        }
+        else{
+            
+            let alert = UIAlertController(title: "Error", message: "Do not use blank charachters", preferredStyle: UIAlertController.Style.alert)
+            
+            let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
     }
     
 
